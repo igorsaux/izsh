c3565d1ca06dbb8ed70a0dad80dc5f8907130593
igorsaux <55196698+igorsaux@users.noreply.github.com>
2025-10-27 23:00:44 +0300

    feat: implement basic shell with prompt and input loop

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,30 @@
 const std = @import("std");
+const zgsh = @import("zgsh");
 
-pub fn main() void {}
+pub fn main() !void {
+    const alloc = std.heap.page_allocator;
+
+    const stdin = std.fs.File.stdin();
+    var stdin_buf: [1024]u8 = undefined;
+    var stdin_reader: std.fs.File.Reader = stdin.reader(&stdin_buf);
+
+    const stdout = std.fs.File.stdout();
+    var stdout_buf: [1024]u8 = undefined;
+    var stdout_writer: std.fs.File.Writer = stdout.writer(&stdout_buf);
+
+    const stderr = std.fs.File.stderr();
+    var stderr_buf: [1024]u8 = undefined;
+    var stderr_writer: std.fs.File.Writer = stderr.writer(&stderr_buf);
+
+    var shell = zgsh.Shell.init(
+        alloc,
+        &stdin_reader.interface,
+        &stdout_writer.interface,
+        &stderr_writer.interface,
+    );
+    defer shell.deinit(alloc);
+
+    while (true) {
+        try shell.execute();
+    }
+}
--- src/zgsh.zig
+++ src/zgsh.zig
@@ -1,1 +1,40 @@
 const std = @import("std");
+
+pub const Shell = struct {
+    stdin: *std.Io.Reader,
+    stdout: *std.Io.Writer,
+    stderr: *std.Io.Writer,
+
+    pub fn init(
+        allocator: std.mem.Allocator,
+        stdin: *std.Io.Reader,
+        stdout: *std.Io.Writer,
+        stderr: *std.Io.Writer,
+    ) @This() {
+        _ = allocator;
+
+        return .{
+            .stdin = stdin,
+            .stdout = stdout,
+            .stderr = stderr,
+        };
+    }
+
+    pub fn execute(this: *@This()) !void {
+        try this.stderr.writeAll("$ ");
+        try this.stderr.flush();
+
+        var byte: [1]u8 = undefined;
+
+        while (byte[0] != '\n') {
+            try this.stdin.readSliceAll(&byte);
+        }
+    }
+
+    pub fn deinit(this: *@This(), allocator: std.mem.Allocator) void {
+        _ = allocator;
+
+        this.stdout.flush() catch {};
+        this.stderr.flush() catch {};
+    }
+};
