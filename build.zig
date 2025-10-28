870e083f6f21e960a5965e20355b4624f9075f36
igorsaux <55196698+igorsaux@users.noreply.github.com>
2025-10-28 17:04:51 +0300

    docs: add MPL 2.0 license and copyright notices

--- build.zig
+++ build.zig
@@ -1,3 +1,9 @@
+// Copyright (C) 2025 Igor Spichkin
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
--- src/zgsh.zig
+++ src/zgsh.zig
@@ -1,3 +1,9 @@
+// Copyright (C) 2025 Igor Spichkin
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 pub const Shell = @import("shell.zig");
 pub const Repl = @import("repl.zig");
 pub const Lexer = @import("lexer.zig");
